5fcca6277b19a3345b473b49a6897ff9bf340310
Ariel Zusman <ariel.zusman@gmail.com>
2018-07-05 14:34:25 +0300

    added makeid fn

--- js/services/utils-service.js
+++ js/services/utils-service.js
@@ -2,45 +2,58 @@
 
 // Save data to local storage
 export function saveToStorage(key, value) {
-	localStorage.setItem(key, JSON.stringify(value))
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
 // Load data from local storage
 export function loadFromStorage(key) {
-	return JSON.parse(localStorage.getItem(key))
+  return JSON.parse(localStorage.getItem(key));
 }
 
 // Delete from local storage
 export function removeFromStorage(key) {
-	localStorage.removeItem(key);
+  localStorage.removeItem(key);
 }
 
 export function getCurrencyIcon(currencyCode) {
-    switch (currencyCode) {
-        case 'EUR':
-            return '€';
-        case 'ILS':
-            return '₪';
-        case 'USD':
-            return '$';
-    }
-};
+  switch (currencyCode) {
+    case 'EUR':
+      return '€';
+    case 'ILS':
+      return '₪';
+    case 'USD':
+      return '$';
+  }
+}
+
+export function makeid(length = 5) {
+  let text = '';
+  let possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
 
 export default {
-    loadFromStorage,
-    saveToStorage,
-    removeFromStorage,
-    getCurrencyIcon
-}
+  loadFromStorage,
+  saveToStorage,
+  removeFromStorage,
+  getCurrencyIcon,
+  makeid
+};
 
 /**
  * Shuffles array in place. ES6 version
  * @param {Array} arr items An array containing the items.
  */
 function shuffle(arr) {
-	for (let i = arr.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[arr[i], arr[j]] = [arr[j], arr[i]];
-	}
-	return arr;
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 }
