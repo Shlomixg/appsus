b3cf860083b52e05d48d5ec5ee12d3c55a696c20
Ariel Zusman <ariel.zusman@gmail.com>
2018-07-07 13:29:26 +0300

    update and save text

--- js/cmps/misterKeep/keep-txt-cmp.js
+++ js/cmps/misterKeep/keep-txt-cmp.js
@@ -3,9 +3,16 @@ export default {
   props: ['data', 'id'],
   template: `
         <section class="keep-txt">
-           <div contenteditable="true">
-               {{data}}
+           <div contenteditable="true" ref="elTxt" v-html="data" @blur="onTxtChange($event)">
            </div> 
         </section>
-    `
+    `,
+  methods: {
+    onTxtChange(ev) {
+      let newData = this.$refs.elTxt.innerHTML;
+
+      console.log(newData);
+      this.$emit('data-changed', { id: this.id, data: newData });
+    }
+  }
 };
