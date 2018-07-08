62941ef2c9ee901006032a5519f9d4a0fb8094a4
Ariel Zusman <ariel.zusman@gmail.com>
2018-07-08 10:54:44 +0300

    buttons for layout

--- js/pages/misterKeep-cmp.js
+++ js/pages/misterKeep-cmp.js
@@ -5,11 +5,15 @@ export default {
   name: 'mister-keep',
   template: `
         <section class="mister-keep container">
+          <div class="controls">
+            <i class="fas fa-grip-horizontal"></i>
+            <i class="fas fa-align-justify"></i>
+            <button class="btn" @click="addNewKeep">New Keep</button>
+          </div>
           <div class="wrapper" v-if="keeps">
             <keep-list :keeps="keeps"></keep-list>    
           </div>
           <router-view :key="$route.fullPath"></router-view>
-          <button class="btn" @click="addNewKeep">New Keep</button>
         </section>
     `,
   data() {
@@ -25,7 +29,7 @@ export default {
   methods: {
     addNewKeep() {
       newKeep().then(keepId => {
-        this.$router.push({ path: `/keep/${keepId}` });
+        this.$router.push({ path: `/keep/${keepId}/details` });
       });
     }
   },
